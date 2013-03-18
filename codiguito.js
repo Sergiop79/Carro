51058197027280bd1fc328da0f70f28e22ed5e13
Sergio Peña <sergiop79@gmail.com>
2013-03-18 20:01:48 +0100

    Se les da funciones al boton contactar y al de nombre, se ha modificado formulario de nombre

--- codiguito.js
+++ codiguito.js
@@ -4,12 +4,17 @@ function inicio ()
 	//Aqui va todo el codigo relacionado con DOM
 	$("#personalizar").on("click", movida);
 	$('#ponnombre').on('click', ponleNombre);
+	$('#contacto').on('click', contactar);
+	$('#cerrarcontacto').on('click', function(){
+		$('#textocontacto').fadeOut('slow');
+	});
 }
 function movida ()
 {
 	//JSON
 	var cambiosCSS =
 	{
+		height: $("#cochecito").height(),
 		margin: 0,
 		overflow: "hidden",
 		padding: 0,
@@ -21,7 +26,6 @@ function movida ()
 		opacity: 1,
 		width: "40%"
 	};
-
 	$("#historia").css(cambiosCSS);
 	$("#personalizacion").css(cambiosPersonalizacion);
 	$("#color div").on("click", cambiarColor);
@@ -33,10 +37,17 @@ function cambiarColor (datos)
 	$("#cochecito img").attr("src", nuevoCoche);
 }
 function ponleNombre(){
-	$('#nombre').toggle(300);
-	$('#nombre').on('keyup', function()	{
-		var $texto = $(this).val();
+	$('#formnombre').slideToggle('fast');
+	$('#boton').on('click', function()	{
+		var $texto = $('#nombre').val();
 		$('h1').html($texto);
 		$('figcaption h2').html($texto);
+		$('#formnombre').fadeOut('fast');
 	});
+	$('#textocontacto').hide();
+
+}
+function contactar(){
+	$('#textocontacto').slideToggle('fast');
+	$('#formnombre').hide();
 }
